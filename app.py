450a9b63b09d7ff8a0bbea65fec50efb6474b02b
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2023-11-14 20:07:36 -0800

    Added CORS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from tictactoe import TicTacToe
 from gamestatus import GameStatus
+from flask_cors import CORS
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/new_game', methods=['GET'])
 def new_game():
